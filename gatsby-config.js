cfa8df604bc1ada22e100394fdda3902fc5fb88a
silverjhany <silverjh95@gmail.com>
2022-03-27 19:11:37 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Hany, Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `hanyJ`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://silverjhany.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      github: `silverjhany`,
     },
   },
   plugins: [
